dc0a21472c82207ccef2cfe8e59ca2969ba245a6
isaneex <isanuric@protonmail.com>
2023-03-26 19:53:54 +0200

    simplify entities

--- src/main/java/com/prismaplaza/shopping/entity/Product.java
+++ src/main/java/com/prismaplaza/shopping/entity/Product.java
@@ -4,29 +4,27 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "products")
 @Data
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
+    @Column(nullable = false)
+    private String name;
 
-        @Column(nullable = false)
-        private String name;
+    @Column(nullable = false)
+    private BigDecimal price;
 
-        @Column(nullable = false)
-        private BigDecimal price;
+    @Column(nullable = false)
+    private LocalDateTime orderDate;
 
-        @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
-        private List<Order> orders = new ArrayList<>();
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
 }
-
-
-
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,29 +1,18 @@
-INSERT INTO public.users (email, password) VALUES
-('user1@example.com', 'password1'),
-('user2@example.com', 'password2'),
-('user3@example.com', 'password3'),
-('user4@example.com', 'password4'),
-('user5@example.com', 'password5');
+INSERT INTO users (email, password) VALUES
+('max@example.com', 'password123'),
+('maria@example.com', 'password456'),
+('peter@example.com', 'password789'),
+('julia@example.com', 'password010'),
+('alex@example.com', 'password011');
 
-INSERT INTO public.products (name, description, price, stock)
-VALUES
-('Tomaten', 'Frische Tomaten aus der Region', 1.99, 50),
-('Gurken', 'Knackige Gurken, ideal als Snack', 0.99, 30),
-('Karotten', 'Süße Karotten mit viel Vitamin A', 1.49, 40),
-('Rote Beete', 'Gesunde Rote Beete für den Salat', 2.29, 20),
-('Kartoffeln', 'Frische Kartoffeln aus dem Acker', 0.79, 80),
-('Zucchini', 'Zarte Zucchini für die leichte Küche', 1.79, 25),
-('Brokkoli', 'Gesunder Brokkoli für die gesunde Ernährung', 2.49, 15),
-('Blumenkohl', 'Fruchtiger Blumenkohl, ideal für Aufläufe', 1.99, 20),
-('Spinat', 'Frischer Spinat für die grüne Küche', 2.99, 10),
-('Champignons', 'Leckere Champignons für die Pfanne', 2.49, 30);
-
-INSERT INTO public.orders (order_date, user_id) VALUES
-('2023-03-25 10:00:00', 1),
-('2023-03-26 11:00:00', 1),
-('2023-03-27 12:00:00', 2);
-
-INSERT INTO public.order_items (quantity, product_id, order_id) VALUES
-(2, 1, 1),
-(3, 1, 2),
-(1, 1, 3);
+INSERT INTO products (name, price, order_date, user_id) VALUES
+('Karotten', 1.99, NOW(), 1),
+('Tomaten', 2.49, NOW(), 1),
+('Gurken', 1.79, NOW(), 2),
+('Zucchini', 3.29, NOW(), 2),
+('Brokkoli', 2.99, NOW(), 3),
+('Blumenkohl', 2.69, NOW(), 3),
+('Spinat', 1.99, NOW(), 4),
+('Kürbis', 2.99, NOW(), 4),
+('Spargel', 4.99, NOW(), 5),
+('Lauch', 1.49, NOW(), 5);
